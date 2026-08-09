1362f5d621252bf20dbb700f61672bd15e7ed278
HilthonTT <timothy.tandt@outlook.be>
2026-08-09 12:47:14 +0200

    updated version

--- src/version.h
+++ src/version.h
@@ -3,7 +3,7 @@
 #define AUDIAKI_VERSION_H
 
 #define AUDIAKI_NAME "audiaki"
-#define AUDIAKI_VERSION "1.0.0"
+#define AUDIAKI_VERSION "2.0.0"
 #define AUDIAKI_HOMEPAGE "https://github.com/HilthonTT/audiaki"
 
 #endif /* AUDIAKI_VERSION_H */
